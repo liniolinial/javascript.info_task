eccf8ac121c9265794a5795d599598e6fce5bb58
Lynn Kim <lynn.kim@brandung.de>
2023-02-22 15:02:44 +0100

    22.02

---
+++ JS/loops.js
@@ -0,0 +1,18 @@
+// let i = 3;
+// while (i) alert(i--);
+
+// let sum = 0;
+// while (true) {
+//   let value = +prompt("Enter a number", '');
+//   if (!value) break; // (*)
+//   sum += value;
+// }
+// alert( 'Sum: ' + sum );
+
+for (let i = 0; i < 10; i++) {
+
+    if (i % 2) {
+      alert( i );
+    }
+  
+  }
