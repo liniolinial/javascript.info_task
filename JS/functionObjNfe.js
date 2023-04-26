515eff402dd5a2516d0ed57b79cb3ebd999659d4
Lynn Kim <lynn.kim@brandung.de>
2023-04-26 19:45:46 +0200

    task2

--- JS/functionObjNfe.js
+++ JS/functionObjNfe.js
@@ -170,30 +170,30 @@
   // console.log(counter());
 }
 // task 1 lösung von mir
-function makeCounter() {
-  let count = 0;
+// function makeCounter() {
+//   let count = 0;
 
-  function counter() {
-    return count--;
-  }
+//   function counter() {
+//     return count--;
+//   }
 
-  function set(value) {
-    count = value;
-  }
-  function decrease() {
-    count--;
-  }
+//   function set(value) {
+//     count = value;
+//   }
+//   function decrease() {
+//     count--;
+//   }
 
-  counter.set = set;
-  counter.decrease = decrease;
+//   counter.set = set;
+//   counter.decrease = decrease;
 
-  return counter;
-}
-let counter = makeCounter();
-counter.set(10);
-console.log(counter());
-console.log(counter());
-console.log(counter());
+//   return counter;
+// }
+// let counter = makeCounter();
+// counter.set(10);
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
 
 //task1 lösung
 // function makeCounter() {
@@ -214,3 +214,37 @@ console.log(counter());
 // console.log(counter());
 // console.log(counter());
 // console.log(counter());
+
+//task2-mein Versuch
+function sumTheNum() {
+  let count = 0;
+
+  function calculator(value) {
+    count += value;
+    return calculator;
+  }
+  calculator.set = (value) => (count = value);
+  return calculator;
+}
+
+let sum = sumTheNum();
+sum.set();
+console.log(sum(1));
+console.log(sum(1)(2)(3));
+
+// task2-Lösung
+// function sum(a) {
+//   let currentSum = a;
+//   function f(b) {
+//     currentSum += b;
+//     return f;
+//   }
+//   f.toString = function () {
+//     return currentSum;
+//   };
+//   return f;
+// }
+// console.log(sum(1)(2).toString()); // 3
+// console.log(sum(5)(-1)(2).toString()); // 6
+// console.log(sum(6)(-1)(-2)(-3).toString()); // 0
+// console.log(sum(0)(1)(2)(3)(4)(5).toString()); // 15
