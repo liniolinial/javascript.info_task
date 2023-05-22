c12d16fc88e3d1bfa5b4564a4d7dbc98e4ae050c
Lynn Kim <lynn.kim@brandung.de>
2023-05-22 14:51:21 +0200

    task4

--- JS/protoInheritance.js
+++ JS/protoInheritance.js
@@ -212,15 +212,64 @@
 // console.log(head.glasses);
 
 //task3
-let animal = {
-  eat() {
-    this.full = true;
+// let animal = {
+//   eat() {
+//     this.full = true;
+//   },
+// };
+
+// let rabbit = {
+//   __proto__: animal,
+// };
+
+// rabbit.eat();
+// //rabbit
+
+// //task4
+// let hamster = {
+//   stomach: [],
+
+//   eat(food) {
+//     this.stomach.push(food);
+//   },
+// };
+
+// let speedy = {
+//   __proto__: hamster,
+//   stomach: [],
+// };
+
+// let lazy = {
+//   __proto__: hamster,
+// };
+
+// // This one found the food
+// speedy.eat("apple");
+// console.log(speedy.stomach); // apple
+
+// // This one also has it, why? fix please.
+// console.log(lazy.stomach); // apple
+
+//task4-lösung
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach = [food];
   },
 };
 
-let rabbit = {
-  __proto__: animal,
+let speedy = {
+  __proto__: hamster,
 };
 
-rabbit.eat();
-//rabbit
+let lazy = {
+  __proto__: hamster,
+};
+
+// This one found the food
+speedy.eat("apple");
+console.log(speedy.stomach); // apple
+
+// This one also has it, why? fix please.
+console.log(lazy.stomach); // apple
