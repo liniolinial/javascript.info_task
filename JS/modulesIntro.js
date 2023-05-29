e4101ca630eb0d6e0750715780acbbf6ce72f57a
Lynn Kim <lynn.kim@brandung.de>
2023-05-29 20:32:08 +0200

    importExport

---
+++ JS/modulesIntro.js
@@ -0,0 +1,39 @@
+// // sayHi.js
+// export function sayHi(user) {
+//     alert(`Hello, ${user}!`);
+//   }
+
+//   // main.js
+// import {sayHi} from './sayHi.js';
+
+// alert(sayHi); // function...
+// sayHi('John'); // Hello, John!
+
+// <!doctype html>
+// <script type="module">
+//   import {sayHi} from './say.js';
+
+//   document.body.innerHTML = sayHi('John');
+// </script>
+
+// export let config = {};
+// export function sayHi() {
+//   alert(`Ready to serve, ${config.user}!`);
+// }
+
+// //import.meta
+// {/* <script type="module">
+//   alert(import.meta.url); // script URL
+//   // for an inline script - the URL of the current HTML-page
+// </script> */}
+
+// //this is undefined
+// <script>
+//   alert(this); // window
+// </script>
+
+// <script type="module">
+//   alert(this); // undefined
+// </script>
+
+//browser specific features
---
+++ JS/modulesIntroHelp.js
@@ -0,0 +1,2 @@
+import { config } from "./JS/modulesIntro.js";
+config.user = "Pete";
