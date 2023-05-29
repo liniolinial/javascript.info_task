c9114a2147651153569168c493dfbd958b9f8fac
Lynn Kim <lynn.kim@brandung.de>
2023-05-29 17:52:54 +0200

    task1-answer

--- JS/tryCatch.js
+++ JS/tryCatch.js
@@ -271,3 +271,17 @@ try {
 }
 
 // cleanup the working space
+
+function func() {
+
+  try {
+    return 1;
+
+  } catch (err) {
+    /* ... */
+  } finally {
+    alert( 'finally' );
+  }
+}
+
+alert( func() ); // first works alert from finally, and then this one
