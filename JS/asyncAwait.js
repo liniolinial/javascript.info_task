e3231deb4862ee5a197e25a530696f8bb604ab7d
Lynn Kim <lynn.kim@brandung.de>
2023-05-30 10:47:10 +0200

    task2

--- JS/asyncAwait.js
+++ JS/asyncAwait.js
@@ -207,6 +207,41 @@
 
 // demoGithubUser();
 
+//zweiter versuch:
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = "HttpError";
+    this.response = response;
+  }
+}
+
+async function loadJson(url) {
+  let response = await fetch(url);
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+async function demoGithubUser() {
+  let name = prompt("Enter a name?", "iliakan");
+  try {
+    let user = await loadJson(`https://api.github.com/users/${name}`);
+    console.log(`Full name: ${user.name}.`);
+    return user;
+  } catch (err) {
+    if (err instanceof HttpError && err.response.status == 404) {
+      console.log("No such user, please reenter.");
+      return demoGithubUser();
+    } else {
+      throw err;
+    }
+  }
+}
+demoGithubUser();
+
 //   / class Thenable {
 //   constructor(num) {
 //     this.num = num;
