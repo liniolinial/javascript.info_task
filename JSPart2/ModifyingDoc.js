8976329493d4f9e3287133c7c76c979a0475ab39
Lynn Kim <lynn.kim@brandung.de>
2023-06-02 01:30:10 +0200

    task6

--- JSPart2/ModifyingDoc.js
+++ JSPart2/ModifyingDoc.js
@@ -4,3 +4,13 @@
 // elem.insertAdjacentText(where, text);
 
 // elem.insertAdjacentElement(where, elem);
+
+// function counter() {
+//   let number = [];
+//   let li = document.getElementsByTagName("li");
+//   document.body.append(ul);
+//   let childCount = li.childElementCount;
+//   let push = number.push(childCount);
+//   return push;
+// }
+// counter();
