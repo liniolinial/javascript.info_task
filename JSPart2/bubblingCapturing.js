259c16bd7c89acd9a13294db216a444a683051dd
Lynn Kim <lynn.kim@brandung.de>
2023-06-09 08:54:56 +0200

    finished

--- JSPart2/bubblingCapturing.js
+++ JSPart2/bubblingCapturing.js
@@ -1,9 +1,21 @@
-form.onclick = function (event) {
-  event.target.style.backgroundColor = "yellow";
+// // ex1
+// form.onclick = function (event) {
+//   event.target.style.backgroundColor = "yellow";
 
-  // chrome needs some time to paint yellow
-  setTimeout(() => {
-    alert("target = " + event.target.tagName + ", this=" + this.tagName);
-    event.target.style.backgroundColor = "";
-  }, 0);
-};
+//   // chrome needs some time to paint yellow
+//   setTimeout(() => {
+//     alert("target = " + event.target.tagName + ", this=" + this.tagName);
+//     event.target.style.backgroundColor = "";
+//   }, 0);
+// };
+
+// // ex2
+
+// for (let elem of document.querySelectorAll("*")) {
+//   elem.addEventListener(
+//     "click",
+//     (e) => alert(`Capturing: ${elem.tagName}`),
+//     true
+//   );
+//   elem.addEventListener("click", (e) => alert(`Bubbling: ${elem.tagName}`));
+// }
