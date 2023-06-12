27e3d84d97775660fe46456cc9c9fd72f5d86e00
Lynn Kim <lynn.kim@brandung.de>
2023-06-12 10:06:39 +0200

    +test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+const PER_VIEW = 3;
+let currentIndex = PER_VIEW;
+
+console.log(PER_VIEW); // Ausgabe: 3
+console.log(currentIndex); // Ausgabe: 3
+
+currentIndex = 5; // Ändern des Wertes von currentIndex
+
+console.log(currentIndex); // Ausgabe: 5
+console.log(PER_VIEW); // Ausgabe: 3
